11bfa011b0e0b31fb29c03fa6ee842f50dd7f27f
Roman Khabarov <roman@ga-games.com>
2020-11-23 21:25:20 +0300

    fix

---
+++ src/components/CellButton/CellButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <VkSimpleCell :getClass="['CellButton', 'CellButton--' + VkPlatform,
+   'CellButton--' + mode,
+   centered === true ? 'CellButton--centered' : ''
+   ]">
+    asd
+  </VkSimpleCell>
+</template>
+
+<script>
+import VkSimpleCell from '@/components/SimpleCell/SimpleCell'
+
+export default {
+  components: { VkSimpleCell },
+  props: {
+    mode: {
+      type: String,
+      default: 'primary'
+    },
+    centered: {
+      type: Boolean,
+      default: false
+    },
+    stopPropagation: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
---
+++ src/components/SimpleCell/SimpleCell.vue
@@ -0,0 +1,26 @@
+<template>
+  <Tappable :getClass="['SimpleCell', 'SimpleCell--' + VkPlatform, ...getClass]">
+    <div class="SimpleCell__main">
+      <div class="SimpleCell__children">
+        <slot/>
+      </div>
+      <div v-if="description" class="SimpleCell__description">{{ description }}</div>
+    </div>
+  </Tappable>
+</template>
+
+<script>
+import Tappable from '@/components/Tappable/Tappable'
+
+export default {
+  components: { Tappable },
+  props: {
+    getClass: {
+      type: Array
+    },
+    description: {
+      type: String
+    }
+  }
+}
+</script>
